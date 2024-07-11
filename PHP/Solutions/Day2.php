7d51ae368d9fae0304684b0224cf937dc2e04a83
Abdullah <Abdallh.a.hatamleh@gmail.com>
2024-07-11 09:48:37 +0300

    loop tasks till 8

--- PHP/Solutions/Day2.php
+++ PHP/Solutions/Day2.php
@@ -90,7 +90,7 @@ function loop3()
         for ($j = 0; $j < 5; $j++)
         {
             echo "*";
-            if(($j + 1 < 5)) echo "  ";
+            if(($j + 1 < 5)) echo "&nbsp;&nbsp;&nbsp;";
         }
         echo "<br>";
     }
@@ -122,8 +122,47 @@ function loop5(int $num)
 // foreach ($variable as $value) {
 //     echo $value . " ,";
 // }
-function loop6(int $num)
-{
-    
+function loop6($str, $char)
+{   
+    $str = strtolower($str);
+    $counter = 0;
+    for ($i = 0; $i < strlen($str); $i++)
+    {
+        if($str[$i] == strtolower($char)) $counter++;
+    }
+    return $counter;
 }
+// echo loop6("Orcange Coding acadecmy","c");
+function loop7()
+{
+    echo "<table cellpadding='3px' cellspacing='0px' border='1px solid'>";
+    for ($i = 1; $i < 11; $i++)
+    {
+        echo "<tr>";
+        for ($j = 1; $j < 11; $j++)
+        {
+            echo "<td> $i * $j = ". ($i * $j) . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+// loop7();
+function loop8(int $num)    
+{
+    echo "<pre>";
+    for ($i = 1; $i < $num + 1; $i++)
+    {
+        if($i % 3 == 0) 
+        {
+            echo "Fizz";
+            if($i % 5 == 0) echo "Buzz";
+        }
+        else if($i % 5 == 0) echo "Buzz";
+        else echo $i;
+        echo "\n";
+        // if($i % 50 == 0) echo "<";
+    }
+}
+// loop8(100000);
 ?>
