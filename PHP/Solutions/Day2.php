ba119e50c29dc4c3e73b5ff57e3ae4d3d16a4d67
Abdullah <Abdallh.a.hatamleh@gmail.com>
2024-07-13 16:03:06 +0300

    moving to pc

--- PHP/Solutions/Day2.php
+++ PHP/Solutions/Day2.php
@@ -164,5 +164,151 @@ function loop8(int $num)
         // if($i % 50 == 0) echo "<";
     }
 }
+function loop9(int $num){
+
+    $len = 1;
+    $inline_count = 0;
+    for ($i = 1; $i <= $num; $i++)
+    {
+        echo $i . " ";
+        $inline_count++;
+        if( $inline_count == $len)
+        {
+            echo "<br>";
+            $len++;
+            // echo "<script> console.log($len)</script>";
+            $inline_count = 0;
+        }
+    }
+}
+// loop9(28)
 // loop8(100000);
+//END OF LOOPS FILE
+function str1(&$str,$operation)
+{
+    switch ($operation)
+    {
+        case "up":
+            $str = strtoupper($str);
+            break;
+        case "low":
+            $str = strtolower($str);
+            break;
+        case "cap":
+            $str[0] = strtoupper($str[0]);
+            break;
+        case "snake":
+            $str = ucwords($str);
+            break;
+        default: 
+        echo "<script> alert('method does not exist');</script>";
+    }
+}
+// $tester = "hello world";
+// str1($tester,"snake");
+// echo $tester;
+function str2(&$str)
+{
+    $str = wordwrap($str, 2, ":", true);
+}
+// $time = "111111";
+// str2($time);
+// echo $time;
+function str3($str,$needle) {
+    if(str_contains($str, $needle))   { 
+    echo "yes it exists";
+    }
+    else echo "no it doesn't";
+}
+// str3("sdfaac orange asdcasd","orange asdw");
+function str4($str) {
+    $temp = explode("/", $str);
+    return $temp[count($temp) -1];
+}
+function str5($str) {
+    
+    $temp = explode("@", $str);
+    return $temp[0];
+}
+function str6($str) 
+{
+    return substr($str,strlen($str) - 3,3);
+
+}
+// echo str6("asfasdfgsdfgasd.com");
+
+function str7($str) {
+return substr(str_shuffle($str),0,str_shuffle("56789")[0]);
+}
+// echo str7("helloWodfgndfghdfg2534rf456rld");
+function str8($str, $toreplace, $new) {
+    return str_replace($toreplace, $new, $str);
+}
+// echo str8("the new trainee", "the", "our");
+function str9($str,$str2) {
+    error_reporting(0);
+    $max = strlen($str);
+    if (strlen($str) < strlen($str2)) {
+        $max = strlen($str2);
+    }
+    for ($i = 0; $i < $max; $i++) {
+        if($str[$i] != $str2[$i]) return $i;
+    }
+}
+function str10($str) {
+    return explode(" ", $str);
+}
+function str11($str) {
+    if (strlen($str) != 1) return "ERROR NOT A SINGLE CHAR";
+    $alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    $curindex = array_search(strtoupper($str), $alphabet);
+    if ($curindex == count($alphabet) - 1) {
+        return "A";
+    }
+    return $alphabet[$curindex + 1];
+}
+
+function str12($str, $toadd, $beginning, $end) {
+    $newsubstr = $beginning . " " . $toadd . " " . $end;
+    $oldsubstr = $beginning . " " . $end;
+    return str_replace($oldsubstr,$newsubstr,$str);
+}
+function str12part2($str) {
+    $temp = explode(" ", $str);
+    return $temp[0];
+}
+function str13($num)
+{
+    $num = strval($num);
+    $num = str_replace("0","",$num);
+    if(str_contains($num,".")) return floatval($num);
+    return intval($num);
+}
+function str14($str, $toremove) {
+    return str_replace($toremove,"",$str);
+}
+
+function str15($str)
+{
+    $temp = "";
+    for ($i = 0; $i < strlen($str) ; $i++)
+    {
+        if($str[$i] != "-") $temp .= $str[$i];
+        else
+        {
+            for ($j = $i; $j < strlen($str); $j++)
+            {
+                if(($str[$j + 1] == "-") || ($j + 1) == strlen($str) ) 
+            }
+        }
+    }
+}
+// echo str15("hello-----");
+// echo str14("the quick brown fox jumps over the lazy dog", "fox");
+// echo str13(20002.225102);
+// echo str12("the brown fox","quick","the","brown");
+// echo str11("h");
+// var_dump( str10("twinkle twinkle little star"));
+// echo str9("hello","hellos");
+// echo str8("hello world idk");
 ?>
